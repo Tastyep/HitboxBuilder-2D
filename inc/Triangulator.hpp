2a89f5e81187dabf069db764fadf4fe442b225da
Luc Sinet <luc.sinet@etixgroup.com>
2017-07-17 21:12:19 +0200

    Update the implementation of the triangulator (ear clipping algo) by finding the most extruded ear each time.

--- inc/Triangulator.hpp
+++ inc/Triangulator.hpp
@@ -1,6 +1,8 @@
 #ifndef HITBOX_BUILDER_TRIANGULATOR_HPP
 #define HITBOX_BUILDER_TRIANGULATOR_HPP
 
+#include <stddef.h>
+#include <utility>
 #include <vector>
 
 #include <SFML/System/Vector2.hpp>
@@ -11,26 +13,42 @@ class Triangulator {
  private:
   using Triangle = std::vector<sf::Vector2i>;
 
- public:
-  Triangulator() = default;
+  struct Vertex {
+    Vertex(sf::Vector2i&& p)
+      : p(std::forward<sf::Vector2i>(p)) {}
 
+    sf::Vector2i p;
+    Vertex* prev;
+    Vertex* next;
+    bool isActive{ true };
+    bool isConvex;
+    bool isEar;
+    float angle;
+  };
+
+ public:
   std::vector<std::vector<sf::Vector2i>> convert(std::vector<sf::Vector2i> polygon) const;
 
  public:
   bool isAngleReflex(const sf::Vector2i& a, const sf::Vector2i& b, const sf::Vector2i& c) const {
-    return (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x) >= 0;
+    return !this->isAngleConvex(a, b, c);
+  }
+  bool isAngleConvex(const sf::Vector2i& a, const sf::Vector2i& b, const sf::Vector2i& c) const {
+    return (c.y - a.y) * (b.x - a.x) - (c.x - a.x) * (b.y - a.y) < 0;
   }
 
  private:
-  std::pair<bool, Triangle> nextEar(std::vector<sf::Vector2i>& polygon) const;
-  bool isPointContained(const Triangle& triangle, const sf::Vector2i& p) const;
+  void initVertices(std::vector<Vertex>& vertices) const;
+  void updateVertex(const std::vector<Vertex>& vertices, Vertex& vertex) const;
+  std::pair<bool, Triangle> nextEar(std::vector<Vertex>& polygon, size_t nbVertices, size_t verticeIndex) const;
+  bool isPointContained(const sf::Vector2i& a, const sf::Vector2i& b, const sf::Vector2i& c,
+                        const sf::Vector2i& p) const;
+  sf::Vector2f normalize(const sf::Vector2i& p) const;
 
  private:
-  bool isPointVertice(const Triangle& triangle, const sf::Vector2i& point) const {
-    return (point == triangle[0] || point == triangle[1] || point == triangle[2]);
-  }
-  bool isAngleReflex(const Triangle& triangle) const {
-    return this->isAngleReflex(triangle[0], triangle[1], triangle[2]);
+  bool isPointVertice(const sf::Vector2i& a, const sf::Vector2i& b, const sf::Vector2i& c,
+                      const sf::Vector2i& point) const {
+    return (point == a || point == b || point == c);
   }
 };
 
--- src/Triangulator.cpp
+++ src/Triangulator.cpp
@@ -1,80 +1,123 @@
 #include "Triangulator.hpp"
 
-#include <iostream>
+#include <cmath>
 #include <tuple>
 
 namespace HitboxBuilder {
 
 std::vector<std::vector<sf::Vector2i>> Triangulator::convert(std::vector<sf::Vector2i> polygon) const {
+  std::vector<Vertex> vertices;
   std::vector<Triangle> triangles;
+  Triangle ear;
   bool success;
-  Triangle triangle;
 
-  do {
-    std::tie(success, triangle) = this->nextEar(polygon);
-    if (success) {
-      triangles.push_back(std::move(triangle));
-    }
-  } while (success);
+  std::move(polygon.begin(), polygon.end(), std::back_inserter(vertices));
+  polygon.clear();
+  this->initVertices(vertices);
+  for (auto& vertex : vertices) {
+    this->updateVertex(vertices, vertex);
+  }
+
+  size_t i = 0;
+  size_t nbVertices = vertices.size();
+  while ((std::tie(success, ear) = this->nextEar(vertices, nbVertices, i)), success) {
+    triangles.push_back(std::move(ear));
+    ++i;
+  }
 
   return triangles;
 }
 
-std::pair<bool, Triangulator::Triangle> Triangulator::nextEar(std::vector<sf::Vector2i>& polygon) const {
-  size_t size = polygon.size();
-  Triangle triangle;
+void Triangulator::initVertices(std::vector<Vertex>& vertices) const {
+  const size_t verticesSize = vertices.size();
 
-  if (size < 3) {
-    return { false, {} };
-  } else if (size == 3) {
-    triangle = { polygon[0], polygon[1], polygon[2] };
-    polygon.clear();
-    return { true, triangle };
-  } else {
-    for (size_t i = 0; i < size; ++i) {
-      bool triTest = false;
-      triangle = { polygon[(i + size - 1) % size], polygon[i], polygon[(i + 1) % size] };
+  for (size_t i = 0; i < verticesSize; ++i) {
+    auto& vertice = vertices[i];
 
-      if (this->isAngleReflex(triangle)) {
-        for (const auto& point : polygon) {
-          if (this->isPointVertice(triangle, point))
-            continue;
-          if (this->isPointContained(triangle, point)) {
-            triTest = true;
-            break;
-          }
-        }
-        if (triTest == false) {
-          polygon.erase(std::next(polygon.begin(), i));
-          return { true, triangle };
-        }
+    vertice.prev = &vertices[(i + verticesSize - 1) % verticesSize];
+    vertice.next = &vertices[(i + 1) % verticesSize];
+  }
+}
+
+void Triangulator::updateVertex(const std::vector<Vertex>& vertices, Vertex& vertex) const {
+  const Vertex& prev = *vertex.prev;
+  const Vertex& next = *vertex.next;
+  const auto vPrev = this->normalize(prev.p - vertex.p);
+  const auto vNext = this->normalize(next.p - vertex.p);
+
+  vertex.angle = vPrev.x * vNext.x + vPrev.y * vNext.y;
+  vertex.isConvex = this->isAngleConvex(prev.p, vertex.p, next.p);
+  vertex.isEar = vertex.isConvex;
+  if (vertex.isConvex) {
+    for (const auto& v : vertices) {
+      if (!this->isPointVertice(prev.p, vertex.p, next.p, v.p) && //
+          this->isPointContained(prev.p, vertex.p, next.p, v.p)) {
+        vertex.isEar = false;
+        break;
       }
     }
   }
-  return { false, {} };
 }
 
-bool Triangulator::isPointContained(const Triangle& triangle, const sf::Vector2i& p) const {
-  std::array<float, 3> barCoef;
-  float eps = 0.0000001;
+std::pair<bool, Triangulator::Triangle>
+Triangulator::nextEar(std::vector<Vertex>& polygon, size_t nbVertices, size_t vertexIndex) const {
+  Triangle triangle;
+  Vertex* ear = nullptr;
 
-  barCoef[0] = ((triangle[1].y - triangle[2].y) * (p.x - triangle[2].x) +
-                (triangle[2].x - triangle[1].x) * (p.y - triangle[2].y)) /
-               (((triangle[1].y - triangle[2].y) * (triangle[0].x - triangle[2].x) +
-                 (triangle[2].x - triangle[1].x) * (triangle[0].y - triangle[2].y)) +
-                eps);
-  barCoef[1] = ((triangle[2].y - triangle[0].y) * (p.x - triangle[2].x) +
-                (triangle[0].x - triangle[2].x) * (p.y - triangle[2].y)) /
-               (((triangle[1].y - triangle[2].y) * (triangle[0].x - triangle[2].x) +
-                 (triangle[2].x - triangle[1].x) * (triangle[0].y - triangle[2].y)) +
-                eps);
-  barCoef[2] = 1.f - barCoef[0] - barCoef[1];
+  if (vertexIndex < nbVertices - 3) {
+    for (size_t j = 0; j < nbVertices; ++j) {
+      auto& v = polygon[j];
 
-  for (float coef : barCoef) {
-    if (coef >= 1.f || coef <= 0.f)
-      return false;
+      if (!v.isActive || !v.isEar) {
+        continue;
+      }
+      if (ear == nullptr || v.angle > ear->angle) {
+        ear = &v;
+      }
+    }
+
+    if (ear == nullptr) {
+      polygon.clear();
+      return { false, {} };
+    }
+
+    triangle = Triangle{ ear->prev->p, ear->p, ear->next->p };
+    ear->isActive = false;
+    ear->prev->next = ear->next;
+    ear->next->prev = ear->prev;
+
+    if (vertexIndex < nbVertices - 4) {
+      this->updateVertex(polygon, *ear->prev);
+      this->updateVertex(polygon, *ear->next);
+    }
+  } else if (vertexIndex == nbVertices - 3) {
+    for (size_t i = 0; i < nbVertices; i++) {
+      const auto& v = polygon[i];
+      if (v.isActive) {
+        triangle = Triangle{ v.prev->p, v.p, v.next->p };
+        break;
+      }
+    }
+  } else {
+    return { false, {} };
   }
-  return true;
+  return { true, std::move(triangle) };
+}
+
+bool Triangulator::isPointContained(const sf::Vector2i& a,
+                                    const sf::Vector2i& b,
+                                    const sf::Vector2i& c,
+                                    const sf::Vector2i& p) const {
+  return !this->isAngleConvex(a, p, b) && //
+         !this->isAngleConvex(b, p, c) && //
+         !this->isAngleConvex(c, p, a);
+}
+
+sf::Vector2f Triangulator::normalize(const sf::Vector2i& p) const {
+  float n = std::sqrt(p.x * p.x + p.y * p.y);
+
+  return (n == 0) ? sf::Vector2f{ 0, 0 } //
+                  : (static_cast<sf::Vector2f>(p) / n);
 }
 
 } /* namespace HitboxBuilder */
