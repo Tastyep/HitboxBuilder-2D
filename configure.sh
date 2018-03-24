01c21f12093bf207dfe361ae0dc3cdfce6ac65f2
Luc Sinet <luc.sinet@etixgroup.com>
2018-03-24 16:31:49 +0100

    #comment Add support for clang-tidy
     - Make available a new target 'clang-tidy' by using the option -y of the configure script

--- configure.sh
+++ configure.sh
@@ -12,9 +12,12 @@ CXX="g++"
 function usage() {
   echo -e "Usage:\n$0 [options]"
   echo -e "\nConfigure a build directory using the provided options."
+  echo -e "\nGeneral options:"
   echo -e "  -h,--help                    Display this help message"
   echo -e "  -c,--clang                   Use clang as compiler"
   echo -e "  -t,--test                    Enable the testbed build target"
+  echo -e "\nClang-specific options:"
+  echo -e "  -y,--tidy                    Enable clang-tidy static analysis target"
   echo
   exit "${1:-1}"
 }
@@ -23,6 +26,7 @@ function configure() {
   cmake \
     -DCMAKE_CXX_COMPILER="$CXX" \
     -DBUILD_TYPE="$HITBOX_BUILDER_BUILD_TYPE" \
+    -DHITBOX_BUILDER_ENABLE_CLANGTIDY="$HITBOX_BUILDER_ENABLE_CLANGTIDY" \
     -DHITBOX_BUILDER_BUILD_TESTBED="$HITBOX_BUILDER_BUILD_TESTBED" \
     "$ROOT_DIR"
 }
@@ -41,11 +45,21 @@ while [[ $# -gt 0 ]]; do
     HITBOX_BUILDER_BUILD_TESTBED="ON"
     shift
     ;;
+  -y | --tidy)
+    HITBOX_BUILDER_ENABLE_CLANGTIDY="ON"
+    shift
+    ;;
   -*)
     echo -e "unknown option '$1'"
     usage
     ;;
   esac
 done
+
+# Prevent use of clang-only options with other compilers
+if [[ "$HITBOX_BUILDER_ENABLE_CLANGTIDY" == "ON" && "$CXX" != "clang++" ]]; then
+  echo -e "cannot use clang-tidy if compiler is not clang"
+  usage
+fi
 
 mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR" && configure
