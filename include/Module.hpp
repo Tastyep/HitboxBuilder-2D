3723795a6f3262b803409fdc8fd8a3a61e8356f5
Luc Sinet <luc.sinet@etixgroup.com>
2018-06-03 17:38:03 +0200

    develop #comment Add documentation on the methods of the module.

--- include/Module.hpp
+++ include/Module.hpp
@@ -9,10 +9,16 @@
 
 namespace HitboxBuilder {
 
+//! Initialize the hitbox builder module.
 void init();
 
+//! Factory function that creates a hitbox from the given sprite.
+//! @param sprite The sprite to build the hitbox from.
+//! @param accuracy a number ranging from 0 to 100.
+//! @param isCartesian A boolean value that defines where to put the origin.
+//! [bottom, left] if @c true, [top, left] if @c false.
+//! @return The hitbox matching the sprite.
 Hitbox make(const sf::Sprite& sprite, size_t accuracy, bool isCartesian);
-
 
 } /* namespace HitboxBuilder */
 
